cd337e43b1b2ea587fe3d129a26f1a0fc3cb5061
dgeorgiev <me@dgeorgiev.biz>
2018-12-30 14:12:31 +0200

    Base layout finished

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,16 +18,8 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <link href='https://fonts.googleapis.com/css?family=Exo:400,500,600' rel='stylesheet' type='text/css'></link>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div className="layout">
           {children}
           <footer>
             © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
