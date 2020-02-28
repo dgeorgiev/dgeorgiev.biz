862f016fa2a5664aa8a0e855c93f7162fedc5183
dgeorgiev <daniel@dabanny.com>
2020-02-28 15:56:20 +0200

    Clean up gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,9 +8,6 @@ const {
 
 exports.onCreatePage = ({ page, actions }) => {
     const { createPage, deletePage } = actions;
-
-    // First delete the incoming page that was automatically created by Gatsby
-    // So everything in src/pages/
     deletePage(page);
 
     Object.keys(locales).map(lang => {
@@ -28,30 +25,13 @@ exports.onCreatePage = ({ page, actions }) => {
         });
     });
 };
-
-// As you don't want to manually add the correct languge to the frontmatter of each file
-// a new node is created automatically with the filename
-// It's necessary to do that -- otherwise you couldn't filter by language
 exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions;
 
-    // Check for "Mdx" type so that other files (e.g. images) are exluded
     if (node.internal.type === `Mdx`) {
-        // Use path.basename
-        // https://nodejs.org/api/path.html#path_path_basename_path_ext
         const name = path.basename(node.fileAbsolutePath, `.mdx`);
-
-        // Check if post.name is "index" -- because that's the file for default language
-        // (In this case "en")
         const isDefault = name === `index`;
-
-        // Find the key that has "default: true" set (in this case it returns "en")
         const defaultKey = findKey(locales, o => o.default === true);
-
-        // Files are defined with "name-with-dashes.lang.mdx"
-        // name returns "name-with-dashes.lang"
-        // So grab the lang from that string
-        // If it's the default language, pass the locale for that
         const lang = isDefault ? defaultKey : name.split(`.`)[1];
 
         createNodeField({ node, name: `locale`, value: lang });
